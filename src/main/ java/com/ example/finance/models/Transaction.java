056603dad52e794f09c4c8de4c86c7ab9f874ff3
salma122-art <salmaahmed125464@gmail.com>
2026-05-05 10:20:51 -0700

    Add abstract Transaction class with properties and methods

--- src/main/ java/com/ example/finance/models/Transaction.java
+++ src/main/ java/com/ example/finance/models/Transaction.java
@@ -1,1 +1,33 @@
 
+package com.example.finance.models;
+
+import java.util.Date;
+
+public abstract class Transaction {
+
+    protected int transactionId;
+    protected double amount;
+    protected Date date;
+    protected String notes;
+
+    public Transaction(int transactionId, double amount, Date date, String notes) {
+        this.transactionId = transactionId;
+        this.amount = amount;
+        this.date = date;
+        this.notes = notes;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public abstract String getDetails();
+}
