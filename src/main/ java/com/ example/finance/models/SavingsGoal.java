a3527efb59eb7006b580acef1ab79e4c3a198ed2
salma122-art <salmaahmed125464@gmail.com>
2026-05-05 10:22:05 -0700

    Update SavingsGoal.java

--- src/main/ java/com/ example/finance/models/SavingsGoal.java
+++ src/main/ java/com/ example/finance/models/SavingsGoal.java
@@ -1,1 +1,42 @@
 
+package com.example.finance.models;
+
+public class SavingsGoal {
+
+    private int goalId;
+    private String targetName;
+    private double targetAmount;
+    private double currentSaved;
+
+    public SavingsGoal(int goalId, String targetName, double targetAmount) {
+        this.goalId = goalId;
+        this.targetName = targetName;
+        this.targetAmount = targetAmount;
+        this.currentSaved = 0.0;
+    }
+
+    public int getGoalId() {
+        return goalId;
+    }
+
+    public String getTargetName() {
+        return targetName;
+    }
+
+    public double getTargetAmount() {
+        return targetAmount;
+    }
+
+    public double getCurrentSaved() {
+        return currentSaved;
+    }
+
+    public double calculateProgress() {
+        if (targetAmount <= 0) return 0;
+        return (currentSaved / targetAmount) * 100;
+    }
+
+    public void addSavings(double amount) {
+        this.currentSaved += amount;
+    }
+}
